fe2fae5751146d66855cd853d66c5c7ffb292ad8
Erick Oliveira <erickoliveira@rorato.local>
2022-04-13 15:59:30 -0300

    Create Project 2

---
+++ backend/src/main/java/com/example/dslearn/entities/User.java
@@ -0,0 +1,67 @@
+package com.example.dslearn.entities;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+        private String name;
+
+        @Column(unique = true)
+        private String email;
+        private String password;
+
+        @ManyToMany(fetch = FetchType.EAGER)
+        @JoinTable(name = "tb_user_role",
+                joinColumns = @JoinColumn(name = "user_id"),
+                inverseJoinColumns = @JoinColumn(name = "role_id"))
+        public Set<Role> roles = new HashSet<>();
+
+        public User() {}
+
+        public User(Long id, String email, String password) {
+            this.id = id;
+            this.email = email;
+            this.password = password;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        public Set<Role> getRoles() {
+            return roles;
+        }
+
+}
