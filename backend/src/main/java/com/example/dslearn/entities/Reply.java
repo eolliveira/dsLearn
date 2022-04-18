f2488e27f45ce9d7c056883c6c8c5473f75d1499
Erick Oliveira <erickoliveira@rorato.local>
2022-04-18 16:13:11 -0300

    Created entities Topic/Reply

---
+++ backend/src/main/java/com/example/dslearn/entities/Reply.java
@@ -0,0 +1,11 @@
+package com.example.dslearn.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+public class Reply implements Serializable {
+    private Long id;
+    private String body;
+    private Instant moment;
+
+}
---
+++ backend/src/main/java/com/example/dslearn/entities/Topic.java
@@ -0,0 +1,15 @@
+package com.example.dslearn.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+public class Topic implements Serializable {
+    private Long id;
+    private String title;
+    private String body;
+    private Instant moment;
+
+    private List<Reply> replies =
+
+
+}
