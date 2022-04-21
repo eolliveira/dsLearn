92c1e7b4aa173fa013fbdaae6c1a824954d1ebd3
unknown <erick.oliveira9607@gmail.com>
2022-04-21 12:10:43 -0300

    Implements exceptional, validation and security to the project

--- backend/src/main/java/com/example/dslearn/entities/User.java
+++ backend/src/main/java/com/example/dslearn/entities/User.java
@@ -1,15 +1,17 @@
 package com.example.dslearn.entities;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.*;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Entity
 @Table(name = "tb_user")
-public class User implements Serializable {
+public class User implements Serializable, UserDetails {
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         private Long id;
@@ -29,7 +31,8 @@ public class User implements Serializable {
 
         public User(Long id, String email, String password) {
             this.id = id;
-            this.email = email;            this.password = password;
+            this.email = email;
+            this.password = password;
         }
 
         public Long getId() {
@@ -56,8 +59,38 @@ public class User implements Serializable {
             this.email = email;
         }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream().map(r -> new SimpleGrantedAuthority(r.getAuthority())).collect(Collectors.toList());
+    }
+
     public String getPassword() {
         return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
     public List<Notification> getNotifications() {
--- backend/src/main/java/com/example/dslearn/services/UserService.java
+++ backend/src/main/java/com/example/dslearn/services/UserService.java
@@ -4,22 +4,40 @@ import com.example.dslearn.dto.UserDTO;
 import com.example.dslearn.entities.User;
 import com.example.dslearn.repositories.UserRepository;
 import com.example.dslearn.services.excepions.ResourcesNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.Optional;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
     @Autowired
     private UserRepository repository;
+
+    private static Logger logger = LoggerFactory.getLogger(UserService.class);
 
     @Transactional
     public UserDTO findById(Long id){
         Optional<User> opt = repository.findById(id);
-        User user = opt.orElseThrow(() -> new ResourcesNotFoundException("Entity id: " + id + " not found"));
+        User user = opt.orElseThrow(() -> new ResourcesNotFoundException("Resource id: " + id + " not found"));
         return new UserDTO(user);
     }
 
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = repository.findByEmail(email);
+        if (user == null) {
+            logger.error("User not found: " + email);
+            throw new UsernameNotFoundException("Email not found");
+        }
+
+        logger.info("User found: " + email);
+        return user;
+    }
 }
